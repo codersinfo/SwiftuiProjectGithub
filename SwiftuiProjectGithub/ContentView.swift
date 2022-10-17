49a1cb9ddd62c7f998cb8ef678c4ccab12530d80
Keerthi Shankar <coders.infoapp@gmail.com>
2022-10-17 11:33:16 +0530

    Initial Commit

---
+++ SwiftuiProjectGithub/ContentView.swift
@@ -0,0 +1,26 @@
+//
+//  ContentView.swift
+//  SwiftuiProjectGithub
+//
+//  Created by Priya Shankar on 17/10/22.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundColor(.accentColor)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ SwiftuiProjectGithub/SwiftuiProjectGithubApp.swift
@@ -0,0 +1,17 @@
+//
+//  SwiftuiProjectGithubApp.swift
+//  SwiftuiProjectGithub
+//
+//  Created by Priya Shankar on 17/10/22.
+//
+
+import SwiftUI
+
+@main
+struct SwiftuiProjectGithubApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
